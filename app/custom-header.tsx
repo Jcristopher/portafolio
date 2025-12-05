ccabb4549c987d3a9375ea9e8f28d48ebdb394a3
RoggerParedes <rparedest@fi.uba.ar>
2025-12-04 21:19:49 -0300

    Add custom header component and update global styles with new color variables

--- app/custom-header.tsx
+++ app/custom-header.tsx
@@ -1,7 +1,14 @@
+import styles from "./custom-header.module.css";
 export default function CustomHeader() {
     return (
-        <header>
-            <h1>Custom Header</h1>
+        <header className={styles.customHeader}>
+            <ul  className={styles.customUl}>
+                <li className={styles.customLi}>Homes</li>
+                <li className={styles.customLi}>About me</li>
+                <li className={styles.customLi}>Projects</li>
+                <li className={styles.customLi}>Contact</li>
+                <li className={styles.customLi}>Skills</li>
+            </ul>
         </header>
     );
 }
