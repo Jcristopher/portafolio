2d011146d0c774bc07129354f3fb1fdc516ad41d
RoggerParedes <rparedest@fi.uba.ar>
2026-01-16 07:57:06 -0300

    feat: Add initial landing page displaying a welcome message, user icon, and URL usage instructions.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { FaCircleUser } from "react-icons/fa6";
+
 export default async function Page() {
     return (
-        <>
-            <h1>Buenas usted debe de ingresar un nombre</h1>
-        </>
+        <div className="m-auto text-center w-[50rem] h-[50rem] flex flex-col justify-center items-center gap-4">
+            <FaCircleUser size={125} />
+            <h1 className="text-4xl font-bold">Bienvenido</h1>
+            <p className="text-xl">Por favor, ingrese un nombre en la URL para continuar</p>
+            <p className="text-xl">Ejemplo: <span className="text-[var(--primary)]">/roalp</span></p>
+        </div>
     );
 }
