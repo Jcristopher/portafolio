85fd4da6a9a4dd77cadcbd07a5c3fff1c5eeac82
RoggerParedes <rparedest@fi.uba.ar>
2026-01-16 08:28:58 -0300

    feat: Add technology image assets and a utility mapper for their paths.

---
+++ app/utils/mapper/technologies.mapper.tsx
@@ -0,0 +1,9 @@
+export const technologiesMapper = {
+    0: "/angular.webp",
+    1: "/react.webp",
+    2: "/html.webp",
+    3: "/css.webp",
+    4: "/next.webp",
+    5: "/react-native.webp",
+    6: "/module-federation.webp",
+}
